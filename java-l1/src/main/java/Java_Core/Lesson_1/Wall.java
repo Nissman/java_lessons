b6392680e96b0c26ba8c6a9f3f83d2b13c545852
Vladislav Ch <sty_ly@mail.ru>
2021-05-18 16:15:06 +0300

    hwl1

--- java-l1/src/main/java/Java_Core/Lesson_1/Wall.java
+++ java-l1/src/main/java/Java_Core/Lesson_1/Wall.java
@@ -1,17 +1,5 @@
 package Java_Core.Lesson_1;
-
+//Стена
 public class Wall implements  Barier{
-    static String type="wall";
-    int distance;
-    public Wall(int distance){
-        this.distance=distance;
-    }
-    @Override
-    public int GetDistance() {
-        return distance;
-    }
-    @Override
-    public String getType() {
-        return type;
-    }
+
 }
